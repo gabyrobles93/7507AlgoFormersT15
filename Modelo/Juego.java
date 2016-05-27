e8e688f4dbc7d6468aa34e132151db0b6c695790
Gabriel Robles <gabyrobles93@gmail.com>
2016-05-27 19:42:33 -0300

    Se comienza a laburar

--- Modelo/Juego.java
+++ Modelo/Juego.java
@@ -2,24 +2,4 @@ package Modelo;
 
 public class Juego {
 
-	public void agregarTablero(Tablero tablero1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void agregarEquipo(Equipo decepticons) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public boolean existeEquipo(Equipo decepticons) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public Tablero getTablero() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
--- Test/AlgoFormerTest.java
+++ Test/AlgoFormerTest.java
@@ -11,25 +11,6 @@ import Modelo.Tablero;
 
 public class AlgoFormerTest {
 	
-	
-	@Test
-	public void testCrearTableroYJugadores(){
-		Juego Juego1=new Juego();
-		Tablero tablero1 = new Tablero();
-		Juego1.agregarTablero(tablero1);
-		Assert.assertTrue(Juego1.getTablero()==tablero1);
-		AlgoFormer megatron=new MegatronAlterno();
-		Equipo Decepticons= new Equipo();
-		Decepticons.agregarAlgoFormer(megatron);
-		Assert.assertTrue(Decepticons.existeAlgoformer(megatron));
-		Juego1.agregarEquipo(Decepticons);
-		Assert.assertTrue(Juego1.existeEquipo(Decepticons));
-		tablero1.ubicarAlgoFormers(Decepticons);
-		Assert.assertTrue(tablero1.existeAlgoformer(megatron));
-	}
-	@Test
-	public void testCrearAlgoFormerConEquipoClaseYUbicacion(){
-		
-	}
+
 
 }
