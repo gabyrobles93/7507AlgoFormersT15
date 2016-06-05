87e593216ca8d11f97747f6d0cb6b5bbfee6adf9
nicolascassia <nicolascassia15@gmail.com>
2016-06-05 19:52:56 -0300

    Nuevo test ObtenerOptimusHumanoide ya pasa

--- Test/AutobotsTest.java
+++ Test/AutobotsTest.java
@@ -7,6 +7,7 @@ import Modelo.Autobots;
 import Modelo.Bumblebee;
 import Modelo.BumblebeeHumanoide;
 import Modelo.Optimus;
+import Modelo.OptimusAlterno;
 import Modelo.OptimusHumanoide;
 import Modelo.Ratchet;
 import Modelo.RatchetHumanoide;
@@ -17,6 +18,18 @@ public class AutobotsTest {
 	@Test
 	public void test01ObtenerOptimusHumanoide(){
 		Optimus optimus=new OptimusHumanoide();
+		Bumblebee bumblebee=new BumblebeeHumanoide();
+		Ratchet ratchet=new RatchetHumanoide();
+
+		Autobots autobots=new Autobots(optimus,bumblebee,ratchet);
+		
+		Assert.assertTrue(optimus==autobots.getOptimus());
+		
+	}
+	
+	@Test
+	public void test02ObtenerOptimusAlterno(){
+		Optimus optimus=new OptimusAlterno();
 		Bumblebee bumblebee=new BumblebeeHumanoide();
 		Ratchet ratchet=new RatchetHumanoide();
 
