b86373fa1e7f630de7fd529098b7c23c99a3e9d1
nicolascassia <nicolascassia15@gmail.com>
2016-06-05 20:27:33 -0300

    Nuevo test ObtenerMegatronAlterno ya pasa

--- Test/DecepticonsTest.java
+++ Test/DecepticonsTest.java
@@ -9,6 +9,7 @@ import Modelo.Decepticons;
 import Modelo.Frenzy;
 import Modelo.FrenzyHumanoide;
 import Modelo.Megatron;
+import Modelo.MegatronAlterno;
 import Modelo.MegatronHumanoide;
 
 
@@ -18,6 +19,18 @@ public class DecepticonsTest {
 	@Test
 	public void test01ObtenerMegatronHumanoide(){
 		Megatron mega=new MegatronHumanoide();
+		Bonecrusher bone=new BonecrusherHumanoide();
+		Frenzy freny=new FrenzyHumanoide();
+
+		Decepticons autobots=new Decepticons(mega,bone,freny);
+		
+		Assert.assertTrue(mega==autobots.getMegatron());
+		
+	}
+	
+	@Test
+	public void test01ObtenerMegatronAlterno(){
+		Megatron mega=new MegatronAlterno();
 		Bonecrusher bone=new BonecrusherHumanoide();
 		Frenzy freny=new FrenzyHumanoide();
 
