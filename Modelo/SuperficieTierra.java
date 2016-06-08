c6e7c63c7d97d419447a30bfd895578faa2f1636
gabyrobles93 <gabyrobles93@gmail.com>
2016-06-07 22:04:43 -0300

    nueva interfaz ''afectable'' para implementar los efectos de la superficie

--- Modelo/SuperficieTierra.java
+++ Modelo/SuperficieTierra.java
@@ -2,4 +2,19 @@ package Modelo;
 
 public abstract class SuperficieTierra extends Superficie{
 
+	int factorvelocidad; 		// pantano
+	int factorataque;			// tormenta psionica
+	int turnosdeespera;			// nebulosa de andromeda
+	int factordañoporespinas;	// espinas
+	
+	public SuperficieTierra(){
+		
+		factorvelocidad = 0;
+		factorataque = 0;
+		turnosdeespera = 0;
+		factordañoporespinas = 0;
+	}
+	
+	public abstract void afectarAlgoformer(Algoformer algoformer);
+
 }
---
+++ Modelo/afectable.java
@@ -0,0 +1,9 @@
+package Modelo;
+
+public interface afectable {
+
+	public void afectarPorEspinas();
+	public void afectarPorPantano();
+	public void afectarPorNebulosaDeAndromeda();
+	public void afectarPorTormentaPsionica();
+}
