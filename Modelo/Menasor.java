467765c6dfca1b599bca9f9b57984f7f2af48c0b
nicolascassia <nicolascassia15@gmail.com>
2016-06-13 12:25:57 -0300

    Se crearon as clases Superion,Menasor,ChispaSuprema,SuperionTest,MenasorTest,ChispaSupremaTest vacias.

---
+++ Modelo/Menasor.java
@@ -0,0 +1,47 @@
+package Modelo;
+
+public class Menasor extends Algoformer{
+
+	@Override
+	public void afectarPorEspinas(double coeficiente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afectarPorPantano(double coeficiente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afectarPorNebulosaDeAndromeda(int cantidadturnos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afectarPorTormentaPsionica(double coeficiente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afectarPorSuperficieRocosa(float coeficiente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afectarPorSuperficieNubosa(float coeficiente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Algoformer cambiarModo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
